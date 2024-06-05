58c9d753cc2c869560d5f880402231e12283d295
Valentine Maduagwu <vchidubem9@gmail.com>
2024-06-05 16:15:36 +0100

    Revised saved keys

--- config/__init__.py
+++ config/__init__.py
@@ -1,9 +1,13 @@
-SAVE_KEYS = set([
-    "Battery_Voltage",
-    "Buck_Converter_Current(A)",
-    "Bus_Voltage",
-    "Output_VA(VA)",
-    "PV_Power(W)",
-    "PV_Voltage(V)",
-    "Output_Watts(W)",
-])
+SAVE_KEYS = set(
+    [
+        "Vbat",
+        "BuckCurr",
+        "BusVolt",
+        "PoutVA",
+        "Ppv",
+        "Vpv",
+        "PoutW",
+    ]
+)
+
+DUPLICATE_KEYS = set(['Vbat'])
